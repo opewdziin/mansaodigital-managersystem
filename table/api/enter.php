281048e3c80f288b6b21898d489d291910f4b786
Pewd <eupewd@gmail.com>
2022-11-14 17:30:36 -0300

    version 0.2

--- table/api/enter.php
+++ table/api/enter.php
@@ -34,37 +34,37 @@ require_once "../../utils/config.php";
     </style>
 </head>
     <body>
-    <table class="table">
-        <thead class="thead-dark">
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Data</th>
-                <th scope="col">Serviço</th>
-                <th scope="col">Valor</th>
-                <th scope="col"></th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-                $link = mysqli_connect("localhost", "root", "", "mansaodigital");
-                $query = mysqli_query($link, "SELECT * FROM services ORDER by id");
-                while($row = $query->fetch_array()) {
-                    echo "<tr>";
-                    echo "<th scope="."row".">".$row['id']."</th>";
-                    $date=date_create($row['datestart']);
-                    echo "<td>" . date_format($date, "d/m/Y") . "</td>";
-                    echo "<td>" . $row['occupation'] . "</td>";
-                    echo "<td>" . $row['pricestart'] . "</td>";
-                    echo "<td><select name="."options".">";
-                    echo "<option value=".""."></option>";
-                    echo "<option value="."change".">Editar</option>";
-                    echo "<option value="."delete".">Excluir</option>";
-                    echo "<option value="."create-exit".">Criar saída</option>";
-                    echo "</select></td>";
-                    echo "</tr>";
-                }
-            ?>
-        </tbody>
-        </table>
+            <table class="table">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Data</th>
+                        <th scope="col">Serviço</th>
+                        <th scope="col">Valor</th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        $link = mysqli_connect("localhost", "root", "", "mansaodigital");
+                        $query = mysqli_query($link, "SELECT * FROM services ORDER by id");
+                        while($row = $query->fetch_array()) {
+                            echo "<tr>";
+                            echo "<th scope="."row".">".$row['id']."</th>";
+                            $date=date_create($row['datestart']);
+                            echo "<td>" . date_format($date, "d/m/Y") . "</td>";
+                            echo "<td>" . $row['occupation'] . "</td>";
+                            echo "<td>" . $row['pricestart'] . "</td>";
+                            echo "<td><select name="."options".">";
+                            echo "<option value=".""."></option>";
+                            echo "<option value="."change".">Editar</option>";
+                            echo "<option value="."delete".">Excluir</option>";
+                            echo "<option value="."create-exit".">Criar saída</option>";
+                            echo "</select></td>";
+                            echo "</tr>";
+                        }
+                    ?>
+                </tbody>
+            </table>
     </body>
 </html>
--- table/index.php
+++ table/index.php
@@ -14,7 +14,6 @@ require_once "../utils/config.php";
 <head>
     <meta charset="UTF-8">
     <title>Mansão Digital - Gerenciador</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         .menu {
             float:left;
@@ -24,14 +23,20 @@ require_once "../utils/config.php";
         }
         .mainContent {
             float:left;
-            width:65%;
-            height:1250px;
+            width: 75vw;
+            height: 1250px;
             border:none;
+        }
+        .container {
+            height: 100vh;
+            width: 100vw;
         }
     </style>
 </head>
     <body>
         <iframe id="choices" class="menu" name="choices" src="./../teste.html"></iframe>
-        <iframe id="main" class="mainContent" name="main" src="./api/enter.php"></iframe>
+        <div class="container">
+            <iframe id="main" class="mainContent" name="main" src="./api/enter.php"></iframe>
+        </div>
     </body>
 </html>
